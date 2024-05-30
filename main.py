417fded732ae188f7be320ecc35e19ee3bdef906
UoA-Naoki <109288033+UoA-Naoki@users.noreply.github.com>
2024-05-30 23:30:05 +0900

    Wild card is now available for path input of create and delete commands.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src import system
 from src import action
 from src import file
+import glob
 
 system.init()
 while True:
@@ -19,7 +20,11 @@ while True:
         if num>=3:
             items=[]
             for item in things[2:]:
-                items.append(item)
+                found=glob.glob(item)
+                if len(found)==0:
+                    items.append(item)
+                items.extend(found)
+            items=list(set(items))
             action.create(things[1],items)
         else:
             print("No such action. Input help for command list.")
@@ -38,8 +43,19 @@ while True:
     elif things[0]=="delete":
         if num>=3:
             items=[]
-            for item in things[2:]:
-                items.append(item)
+            if things[1]=="-i":
+                for item in things[2:]:
+                    items.append(item)
+            elif things[1]=="-p":
+                for item in things[2:]:
+                    found=glob.glob(item)
+                    if len(found)==0:
+                        items.append(item)
+                    items.extend(found)
+                items=list(set(items))
+            else:
+                print("No such action. Input help for command list.")
+                continue
             action.delete(things[1],items)
         else:
             print("No such action. Input help for command list.")
